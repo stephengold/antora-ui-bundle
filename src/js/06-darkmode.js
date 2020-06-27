c7ce11fd1454ca32718dccf3dc8ad7e50c788ce9
Stephen Gold <sgold@sonic.net>
2020-06-26 23:18:54 -0700

    replace the theme-switch slider with a drop-down menu

--- src/js/06-darkmode.js
+++ src/js/06-darkmode.js
@@ -1,25 +1,12 @@
-var toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]')
 var currentTheme = window.localStorage.getItem('theme')
-
 if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme)
-  if (currentTheme === 'dark') {
-    toggleSwitch.checked = true
-  }
 } else {
   //set dark by default
   document.documentElement.setAttribute('data-theme', 'dark')
-  toggleSwitch.checked = true
 }
 
-function switchTheme (e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark')
-    window.localStorage.setItem('theme', 'dark')
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light')
-    window.localStorage.setItem('theme', 'light')
-  }
+function setTheme(x) {
+    document.documentElement.setAttribute('data-theme', x)
+    window.localStorage.setItem('theme', x)
 }
-
-toggleSwitch.addEventListener('change', switchTheme, false)
